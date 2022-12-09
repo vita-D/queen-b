6f72ac4d2c4823d417b56c9fa4b2e28ebf8cd4a6
vitalina Blokh <117175472+vita-D@users.noreply.github.com>
2022-12-09 13:30:28 +0000

    init commit

---
+++ 3-myProject.js
@@ -0,0 +1,25 @@
+function game(){
+  //👇התחילי לכתוב את הפרויקט כאן למטה
+var ochenka = 0;
+var ochenka = Number(ochenka);
+ alert(" Hi, it's a friendship test");
+ var ima = prompt(" What is my name? ");
+ if (ima == "Vitalina") { ochenka += 1; 
+ }if (ima == "vitalina") { ochenka += 1; 
+ }else { alert(" Really?... ");}
+  var vs = prompt( "How old I am?");
+  if (vs == "13") { ochenka += 1;  
+  }else { alert(" No. "); }
+  var br = prompt(" Do I have brother?");
+if (br == "Yes") { ochenka += 1; 
+}if (br == "yes") { ochenka += 1; 
+}else { alert(" sadly... ");}
+  var hz = prompt(" My favorit color? ");
+  if (hz == "Blue") { ochenka += 1; 
+  }else if (hz == "blue") { ochenka += 1; 
+  }else if (hz == "black") { ochenka += 1; 
+  }else if (hz == "Black") { ochenka += 1;                     
+  }else { alert(" Nope. ");}
+alert(" Soo, the test ended. What is your grade?");
+alert(" It's " + ochenka + "/4 !");     
+}
